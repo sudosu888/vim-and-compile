31f1acb552f7cefcea4d9e0ef04d6b319da3761d
sudosu888 <zebbieman123@gmail.com>
2020-04-15 21:31:50 -0600

    added vc.c, I forgot to add it on the first commit because I'm a dummy

---
+++ vc.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main(int argc, char *argv[])
+{
+	/*
+	 * Needed variables and integers
+	 */
+	int arg;
+	char *file = argv[1];
+	char edit[64];
+	char gcc[64];
+	char cc[64];
+
+	/*
+	 * System commands
+	 */
+	sprintf(edit, "vim %s", file);
+	sprintf(gcc, "gcc %s", file);
+	sprintf(cc, "cc %s", file);
+
+	/*
+	 * command line arguments
+	 * -e edit only
+	 * -g compile with gcc (instead of cc)
+	 *  default: Error out
+	 */
+	while((arg = getopt(argc, argv, "eg")) != -1)
+	{
+		switch(arg)
+		{
+			case 'e' :
+				system(edit);
+				return 0;
+			case 'g' :
+				system(edit);
+				system(gcc);
+				return 0;
+			default :
+				printf("Invalid arguments\n");
+				return 0;
+		}
+	}
+	
+	system(edit);
+	system(cc);
+	return 0;
+}
